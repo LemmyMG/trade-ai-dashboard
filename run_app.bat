a310c2dce9cb9338b5a0909e154a2c110baa2b93
LMG <lemmymgee@gmail.com>
2026-03-13 13:26:27 +0100

    Delete run_app.bat

--- run_app.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-echo Starting Backend Server...
-start cmd /k "cd /d %~dp0 && uvicorn app:app --reload"
-
-timeout /t 3
-
-echo Starting Frontend Dashboard...
-start cmd /k "cd /d %~dp0 && streamlit run frontend.py"
